cf38768c657870f8562a657c605e03c1904e4077
Maneesha09-87 <ramapurammaneesha@gmail.com>
2024-04-16 21:18:14 +0530

    Learning variables in JS

---
+++ index.js/index.js
@@ -0,0 +1,28 @@
+//JAVASCRIPT PRIMITIVES
+//STRING 
+let name ="jonathan";
+//NUMBER
+
+//OBJECTS:keys and values
+let obj={name:'maneesha',
+  age:21}
+console.log(obj.name)
+//ARRAYS
+let arr=['maneesha','jhanu',0,'Nishu']
+//objects
+let user={
+  name:'Maneesha',
+  rollnumber:235,
+  collegename:"MTIET",
+  state:"Andhra Pradesh",
+  country:"India"}
+console.log(user.name)
+console.log(user.rollnumber)
+console.log(user.collegename)
+console.log(user.state)
+console.log(user.country)
+let arr1=['mani','jhanu','Nishu']
+console.log(arr1[0])
+console.log(arr1[1])
+console.log(arr1[2])
+
